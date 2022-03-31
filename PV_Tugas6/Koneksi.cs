564482836fbe10642e8b3ffa665de8596fc661d7
Cheliza Sriayu Simarsoit <77439245+chelizaaa@users.noreply.github.com>
2022-03-31 14:14:09 +0700

    Add files via upload

---
+++ PV_Tugas6/Koneksi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// 1
+using System.Data.SqlClient;
+
+namespace PV_Tugas6
+{
+    class Koneksi
+    {
+        // 2
+        public SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source = CHELIZA-SRIAYU; initial catalog = Kasir; integrated security = true";
+            return Conn;
+        }
+    }
+}
